81eda5a3571132fe48d90af35efcbcd60a398128
Emre Demirkan <emredemirkan99@outlook.com>
2025-07-16 16:40:18 +0300

    Proje dosyası ekle.

---
+++ MCPServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(settings: null);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes your first name back to the client.")]
+    public static string EchoFirstName(string firstName) => $"Hello {firstName}!";
+}
